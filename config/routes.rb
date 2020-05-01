25871eae8be60b598ded2ae66d11da1421621fc8
umekinomori <umeki7538@icloud.com>
2020-05-01 20:01:04 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  devise_for :users
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  
 end
